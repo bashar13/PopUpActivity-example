fb20dec68a554bc94147e5c972b4c1c36f689ae4
Jahid Bashar <Jahid@appleui-MacBook-Pro-5.local>
2018-08-14 17:43:10 -0600

    Initial source commit

---
+++ app/src/main/java/com/bashar/popupactivity/PopUpActivity.java
@@ -0,0 +1,104 @@
+package com.bashar.popupactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class PopUpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature (Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_popup);
+
+    }
+
+    public void clearPressed(View v)
+    {
+
+    }
+    public void onePressed(View v)
+    {
+
+    }
+
+    public void twoPressed(View v)
+    {
+
+    }
+
+    public void threePressed(View v)
+    {
+
+    }
+
+    public void fourPressed(View v)
+    {
+
+    }
+
+    public void fivePressed(View v)
+    {
+
+    }
+
+    public void sixPressed(View v)
+    {
+
+    }
+
+    public void sevenPressed(View v)
+    {
+
+    }
+
+    public void eightPressed(View v)
+    {
+
+    }
+
+    public void ninePressed(View v)
+    {
+
+    }
+
+    public void zeroPressed(View v)
+    {
+
+    }
+
+    public void minusPressed(View V)
+    {
+
+    }
+
+    public void equalPressed(View V)
+    {
+
+    }
+
+    public void dotPressed(View V)
+    {
+
+    }
+
+    public void multiPressed(View V)
+    {
+
+    }
+
+    public void plusPressed(View V)
+    {
+
+    }
+
+    public void dividePressed(View V)
+    {
+
+    }
+}
